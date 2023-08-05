86bf67afc7608adb84b468a4e554ac12711512a0
Andy Truong <andytruongcs@gmail.com>
2023-08-05 14:25:28 -0400

    Simplified code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -32,27 +29,21 @@ function App() {
 
   return (
     <>
-      <Container>
-        <Row>
-          <Col className='d-flex justify-content-center align-items-center vh-100'>
-            <Card className='bg-light'>
-              <Card.Body className='text-center my-4'>
-                <Card.Text>
-                  "{quote}"
-                </Card.Text>
-                <Card.Text className='text-end'>
-                  --- {author}
-                </Card.Text>
-                <Button className='bg-navy mt-3' onClick={getRandomQuote}>
-                  Generate Random Quote
-                </Button>
-
-
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div className='d-flex justify-content-center align-items-center vh-100'>
+        <Card className='bg-light'>
+          <Card.Body className='text-center my-4'>
+            <Card.Text>
+              "{quote}"
+            </Card.Text>
+            <Card.Text className='text-end'>
+              --- {author}
+            </Card.Text>
+            <Button className='bg-navy mt-3' onClick={getRandomQuote}>
+              Generate Random Quote
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
 
     </>
   );
